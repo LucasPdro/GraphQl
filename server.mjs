08422bc2a9d0e4b14cf1c5bcfbeeb284b110ce11
Lucas Pedro <lucaspedro_2014@hotmail.com>
2025-09-26 09:46:31 -0300

    initial

---
+++ server.mjs
@@ -0,0 +1,21 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { gql } from "graphql-tag";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Olá, Lucas! GraphQL funcionando 🎉",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
+
+console.log(`🚀 Servidor rodando em ${url}`);
